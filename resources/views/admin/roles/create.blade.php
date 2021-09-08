f3e39396f40456edc67e6c40ce0ddeff74697198
Nguyễn Trung Dũng <63592403+trdung1999@users.noreply.github.com>
2021-09-08 18:33:13 +0700

    Update create.blade.php

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -5,6 +5,12 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 @endpush
 @section('content')
+<style>
+    .select2-selection--multiple{
+        overflow: hidden;
+        height: auto !important;
+        }
+</style>
 <div class="container-fluid add-form-list">
     <div class="row">
         <div class="col-sm-12">
